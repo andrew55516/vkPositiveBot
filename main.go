c6c750d375cdfdf3a017d24b4b735f37f715b358
Andrey Aksenov <aksenovandrey4@gmail.com>
2023-05-06 15:36:27 +0300

    Initial commit

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ const ()
 
 func main() {
 	http.HandleFunc("/callback", hadleCallback)
-	log.Fatal(http.ListenAndServe(":5986", nil))
+	log.Fatal(http.ListenAndServe(":443", nil))
 }
 
 func hadleCallback(w http.ResponseWriter, r *http.Request) {
